2ed2d2a6261b9e285769450a996e313a54b50bb6
JohnYHMM <johnyehtetmgmg@gmail.com>
2023-02-19 08:50:26 +0630

    [Back-End] Add Empty Project

---
+++ back-end/src/main/java/com/john/massplanning/MassPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.john.massplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassPlanningApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/john/massplanning/MassPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.massplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
