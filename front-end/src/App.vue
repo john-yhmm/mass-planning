51f455033e10b75dfa72cc25e036ffe3e8d471d6
JohnYHMM <johnyehtetmgmg@gmail.com>
2023-02-19 09:43:15 +0630

    [Front-End] Add Project Frame

---
+++ front-end/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-app-bar :elevation="2">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Roman Catholic Mass Planning</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" temporary></v-navigation-drawer>
+    <v-main>
+      <Home />
+    </v-main>
+</v-app>
+</template>
+
+<script setup>
+import Home from '@/components/Home.vue'
+import { ref } from 'vue';
+
+let drawer = ref(false);
+</script>
---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid class=" rounded">
+    <v-row justify="center" v-for="n in 3" :key="n" no-gutters>
+      <v-col cols="6" sm="5" md="4" lg="3">
+        <v-sheet class="ma-2 pa-2">My Label</v-sheet>
+      </v-col>
+      <v-col cols="6" sm="5" md="4" lg="3">
+        <v-text-field variant="underlined" hide-details></v-text-field>
+      </v-col>
+    </v-row>
+</v-container>
+</template>
+<script>
+export default {
+
+}
+</script>
